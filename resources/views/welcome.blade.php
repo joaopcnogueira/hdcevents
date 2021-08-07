8f204dbc397bec2e44bd32ecd809b4f4851db94c
João Nogueira <joaonogueira@fisica.ufc.br>
2021-08-07 17:15:40 -0300

    layouts com blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,51 +1,36 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
 
-        <title>Laravel</title>
+@section('title', 'HDC Events')
 
-        {{-- Adicionando arquivo css --}}
-        {{-- <link rel="stylesheet" href="/css/styles.css"> --}}
-        <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-        {{-- Adicionando arquivo js --}}
-        {{-- <script src="/js/scripts.js"></script> --}}
-        <script src="{{ asset('js/scripts.js') }}"></script>
+@section('content')
+    <h1>Algum título</h1>
+    <img src="{{ asset('img/banner.jpg') }}" alt="Banner">
 
-    </head>
-    <body>
+    {{-- Diretivas do blade e variáveis --}}
+    @if ($nome == "João")
+        <p>O nome é {{ $nome }} e ele tem {{ $idade }} anos e trabalha como {{ $profissao }}</p>
+    @elseif ($nome == "Matheus")
+        <p>O nome é {{ $nome }} e ele tem {{ $idade }} anos e trabalha como {{ $profissao }}</p>
+    @else
+        <p>O nome não é {{ $nome }} e ele tem {{ $idade }} anos e trabalha como {{ $profissao }}</p>
+    @endif
 
-        <h1>Algum título</h1>
-        <img src="{{ asset('img/banner.jpg') }}" alt="Banner">
+    @foreach ($arr as $item)
+        <ul>
+            <li>{{ $item }}</li>
+        </ul>
+    @endforeach
 
-        {{-- Diretivas do blade e variáveis --}}
-        @if ($nome == "João")
-            <p>O nome é {{ $nome }} e ele tem {{ $idade }} anos e trabalha como {{ $profissao }}</p>
-        @elseif ($nome == "Matheus")
-            <p>O nome é {{ $nome }} e ele tem {{ $idade }} anos e trabalha como {{ $profissao }}</p>
-        @else
-            <p>O nome não é {{ $nome }} e ele tem {{ $idade }} anos e trabalha como {{ $profissao }}</p>
-        @endif
+    {{-- Executando código PHP direto na view --}}
+    @php
+        $texto = "PHP direto na View com Blade";
+        echo $texto;
+    @endphp
 
-        @foreach ($arr as $item)
-            <ul>
-                <li>{{ $item }}</li>
-            </ul>
-        @endforeach
+    <!-- Comentário do HTML: aparece na View-->
+    {{-- Comentário do Blade: Não aparece na View --}}
 
-        {{-- Executando código PHP direto na view --}}
-        @php
-            $texto = "PHP direto na View com Blade";
-            echo $texto;
-        @endphp
-
-        <!-- Comentário do HTML: aparece na View-->
-        {{-- Comentário do Blade: Não aparece na View --}}
-
-        @foreach ($nomes as $nome)
-            <p>{{ $loop->index}} -> {{ $nome }}</p>
-        @endforeach
-
-    </body>
-</html>
+    @foreach ($nomes as $nome)
+        <p>{{ $loop->index}} -> {{ $nome }}</p>
+    @endforeach
+@endsection
