83ee99abf4ef668bfd3c83fc2a320f628ef9b4d3
João Nogueira <joaonogueira@fisica.ufc.br>
2021-08-11 22:48:00 -0300

    campo de upload de imagem adicionado

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -25,6 +25,16 @@ class EventController extends Controller
         $event->private = $request->private;
         $event->description = $request->description;
 
+        // Image Upload
+        if($request->hasFile('image') and $request->file('image')->isValid()) {
+            $requestImage = $request->image;
+            $extension = $requestImage->extension();
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . '.' . $extension;
+
+            $requestImage->move(public_path('img/events'), $imageName);
+            $event->image = $imageName;
+        }
+
         $event->save();
 
         return redirect()
