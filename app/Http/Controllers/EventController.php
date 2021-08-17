0b08d9b46ca0957f956e6474578c2cddb0cc7273
João Nogueira <joao.nogueira@wittel.com>
2021-08-17 18:43:50 -0300

    dashboard finalizado

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -67,4 +67,12 @@ class EventController extends Controller
         return view('events.show', compact('event', 'eventOwner'));
         
     }
+
+	public function dashboard()
+	{
+        $user = auth()->user();
+        $events = $user->events;
+
+        return view('events.dashboard', compact('events'));
+	}
 }
